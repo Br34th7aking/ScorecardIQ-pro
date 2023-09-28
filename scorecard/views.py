6ba7de75c649d50ad522a3b83ca30b027c96ef0f
Br34th7aking <abhijit18231@iiitd.ac.in>
2023-09-28 11:13:30 +0530

    Set up the Django app and the ETL code to add team information in the database

--- scorecard/views.py
+++ scorecard/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Teams(APIView):
+    """
+    List all teams
+    """
+    def get(self, request, format=None):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
